cdc1535266ac4716255a43d5f0698303901c43e7
upchaudhari <upasana@pdx.edu>
2023-06-07 20:21:13 -0700

    Final formatting change in javascript

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const script = document.createElement("script");
 script.src = "https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js";
 document.head.appendChild(script);
 script.onload = function () {
-  const typed = new Typed("#element", {
+  new Typed("#element", {
     strings: [
       "CS Grad Student.",
       "Graduate Admin Assistant.",
