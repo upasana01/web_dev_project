e90adf71ef980d35e360c2ba5b15d493699c3a50
upchaudhari <upasana@pdx.edu>
2023-06-07 20:08:44 -0700

    Final Minor Changes Done. Ready to be Deployed

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-var script = document.createElement("script");
+const script = document.createElement("script");
 script.src = "https://unpkg.com/typed.js@2.0.16/dist/typed.umd.js";
 document.head.appendChild(script);
 script.onload = function () {
-  var typed = new Typed("#element", {
+  const typed = new Typed("#element", {
     strings: [
       "CS Grad Student.",
       "Graduate Admin Assistant.",
